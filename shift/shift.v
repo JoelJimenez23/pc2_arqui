b2df4cd1a7d5001f38ae992d0cdaae2c3588eab5
herbert54 <90295219+herbert54@users.noreply.github.com>
2024-11-14 23:11:08 -0500

    Update shift.v

--- shift/shift.v
+++ shift/shift.v
@@ -5,8 +5,10 @@ module shifter(input  [31:0] a,
  
   always @(*) begin
     case (shtype) 
-      2'b00:   y = a << shamt; 
+      	  2'b00:   y = a << shamt; 
 	  2'b01:   y = a >> shamt;
+	  2'b10:   y = $signed(a) >>> shamt;
+	  2'b11:   y = (a >> shamt ) | (a << (32- shamt));
       default: y = a; 
     endcase 
   end
